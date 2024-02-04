3f0c401265f206bfde0a561b7c2b6ab7e602629c
venkatraghavan08 <venkatraghavan3456@gmail.com>
2024-02-04 13:33:00 +0000

    CCBP Timeline

---
+++ src/components/CourseTimelineCard/index.js
@@ -0,0 +1,26 @@
+// Write your code here
+import {AiFillClockCircle} from "react-chrono/ai"
+import index.css
+
+const CourseTimeLineCard=props=>{
+    const {data}=props
+    return(
+        <div className="card-container">
+            <div className="title-duration-container">
+                <h1 className="title">{data.courseTitle}</h1>
+                <div className="calender-duration">
+                    <AiFillClockCircle/>
+                    <p className="duration">{data.duration}</p>
+                </div>
+            </div>
+            <p className="project-description">{data.description}</p>
+            <div className="tags-container">
+                {data.tagsList.map(eachItem=>(
+                <p className="tags" key={eachItem.id}>{eachItem.name}{' '}
+                </p>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default CourseTimeLineCard
---
+++ src/components/ProjectTimelineCard/index.js
@@ -0,0 +1,25 @@
+// Write your code here
+import {AiFillCalender} from 'react-icons/ai'
+import './index.css'
+
+const ProjectTimeLineCard = props => {
+  const {data} = props
+  return (
+    <div className="card-container">
+      <img className="image" alt="project" src={data.imageUrl} />
+      <div className="title-duration-container">
+        <h1 className="title">{data.projectTitle}</h1>
+        <div className="calender-duration">
+          <AiFillCalender />
+          <p className="duration">{data.duration}</p>
+        </div>
+      </div>
+      <p className="project-description">{data.description}</p>
+      <a className="visit-link" href={data.projectUrl}>
+        {' '}
+        Visit{' '}
+      </a>
+    </div>
+  )
+}
+export default ProjectTimeLineCard
